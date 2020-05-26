4836853db981f7cf5efce4dbc965edfc435f62bc
Vinicius Oliveira <vinicius.vof@outlook.com>
2020-05-26 14:16:24 -0300

    Update FrequencyController.js

--- src/controllers/FrequencyController.js
+++ src/controllers/FrequencyController.js
@@ -3,7 +3,7 @@ const moment = require('moment-timezone');
 
 module.exports = {
     async index(req, res) {
-        const data = moment.tz("America/Sao_Paulo");
+        const data = moment.tz("America/Sao_Paulo").format("DD-MM-YYYY");;
         const alunos = await connection('frequency')
             .join('students', 'students.id', '=', 'frequency.student_id')
             .join('class', 'class.id', '=', 'frequency.class_id')
